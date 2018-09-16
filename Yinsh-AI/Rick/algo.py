e34e58b58fd89d8ed1f6b580c67fa044db4f8b55
Ankit Solanki <ankit03june@gmail.com>
2018-09-16 12:46:21 +0530

    Debugging min max algorithm - cleaned depth issues

--- Yinsh-AI/Rick/algo.py
+++ Yinsh-AI/Rick/algo.py
@@ -4,19 +4,22 @@ from board import Board
 
 class Algo:
 	def __init__(self):
-		pass
+		self.depth = 2
 
 	def print_alp_bet(self, alp, bet):
 		print("alpha: ", alp, ":: beta: ", bet)
 		return True
 
 	def min_max(self, board):
-		self.depth = 0
+		depth = 0
 		alpha_init = sys.float_info.min
 		beta_init = sys.float_info.max
-		self.print_alp_bet(alpha_init, beta_init)
+		# self.print_alp_bet(alpha_init, beta_init)
 
-		child_val = self.max_value(board, alpha_init, beta_init)
+		child_val = self.max_value(board, alpha_init, beta_init, depth)
+		# print("child val: ", child_val)
+
+		return True
 		gc.collect() # collect garbage here
 
 		try:
@@ -28,46 +31,51 @@ class Algo:
 			print("[*] Value not found in neighbours!")
 			raise ValueError
 
-	@property
-	def is_terminal(self):
-		return self.depth >= 4
+	def is_terminal(self, depth):
+		return depth >= self.depth
 
-
-	def max_value(self, board, alpha, beta):
-		if self.is_terminal:
+	def max_value(self, board, alpha, beta, depth):
+		if self.is_terminal(depth):
 			return board.utility
 		
-		self.depth += 1
-
 		neighbs = board.get_neighbours()
-		# for b in neighbs:
-		# 	child_value = self.min_value(b, alpha, beta)
-		# 	alpha = min(alpha, child_value)
+		for (i, b) in enumerate(neighbs):
+			print("--------------------------", i, "-------------------------------")
+			# print("[*] ", depth)
+			child_value = self.min_value(b, alpha, beta, depth+1)
+			# self.print_alp_bet(alpha, beta)
+			# print("child val", child_value)
 
-		# 	if alpha>=beta:
-		# 		return child_value
-
-		max_util =  max([x.utility for x in neighbs])
-
-		print(max_util)
-
-
-	def min_value(self, board, alpha, beta):
-		if self.is_terminal:
-			return utility.func(board)
-
-		self.depth += 1
-
-		neighbs = board.get_neighbours()
-		for b in neighbs:
-			child_value = self.max_value(b, alpha, beta)
-			beta = min(beta, child_value)
-
+			alpha = max(alpha, child_value)
+			# print("alpha-dash", alpha)
 			if alpha>=beta:
 				return child_value
 
-		min_util =  min([utility.func(x) for x in neighbs])
+		max_util =  max([x.utility for x in neighbs])
 
+		# print("max-util", max_util)
+		return max_util
+
+
+	def min_value(self, board, alpha, beta, depth):
+		if self.is_terminal(depth):
+			return board.utility
+
+		neighbs = board.get_neighbours()
+		for (i, b) in enumerate(neighbs):
+			print(i)
+			# print("[*] ", depth)
+			child_value = self.max_value(b, alpha, beta, depth+1)
+			# self.print_alp_bet(alpha, beta)
+			# print("child val", child_value)
+			beta = min(beta, child_value)
+			# print("beta-dash", beta)
+			if alpha>=beta:
+				return child_value
+
+		min_util =  min([x.utility for x in neighbs])
+
+		# print("min-util", min_util)
 		return min_util
 
 if __name__ == '__main__':
